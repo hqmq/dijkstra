defb5fc34441329906ce88521e5ebce4f9d151c7
Michael Ries <michael@riesd.com>
2012-04-18 23:16:38 -0600

    got the graph drawing the nodes

--- coffee/dijkstra.coffee
+++ coffee/dijkstra.coffee
@@ -1,23 +1,76 @@
 window.onload = () ->
-	paper = Raphael("holder", 400, 400)
-	graph = new Graph(3,3)
+	paper = Raphael("holder", 800, 600)
+	graph = new Graph(paper)
 	window.graph = graph
-	circle = paper.circle(50, 40, 10);
-	circle.attr("fill", "#f00");
-	circle.attr("stroke", "#fff");
+	
+	n1 = new Node(2,4)
+	n1.origin = true
+	n2 = new Node(4,3)
+	n3 = new Node(2,2)
+	n4 = new Node(4,1)
+	n5 = new Node(2,0)
+	n5.target = true
+	n6 = new Node(0,1)
+	n1.addEdge(7,n2)
+	n1.addEdge(14,n6)
+	n1.addEdge(9, n3)
+	
+	graph.addNode( node ) for node in [n1,n2,n3,n4,n5,n6]
+	graph.draw()
 	
 class Graph
-	constructor: (width, height, )->
-		#fill out a list of nodes
-		@nodes = [1..height].map (row) ->
-			[1..width].map (col) ->
-				new Node(this)
-				
-		@origin = @nodes[0][0]
-		@target = @nodes[height][width]
+	constructor: (@raphael)->
+		@nodes = []
+		@origin = false
+		@target = false
+		
+	addNode: (node)->
+		@nodes.push(node)
+		if node.origin
+			@origin = node
+		if node.target
+			@target = node
+		
+	draw: (current = false)->
+		this.drawNode(node) for node in @nodes
+		
+	drawNode: (node) ->
+		x = node.x * 65 + 65
+		y = node.y * 65 + 65
+		circle = @raphael.circle(x, y, 30)
+		if node == @origin
+			color = "#fff"
+		else if node == @target
+			color = "#f00"
+		else
+			color = '#00f'
+			
+		circle.attr('fill', color)
+		circle.attr('stroke', color)
 		
 class Node
-	constructor: (@graph)->
+	constructor: (@x, @y)->
 		@visited = false
-		@distance = -1
+		@distance = Infinity
+		@edges = {}
+		@origin = false
+		@target = false
+	addEdge: (distance, node)->
+		@edges[node] = distance
+	
+		
+class Dijkstra
+	constructor: (@graph) ->
+		@current = graph.origin
+		@curent.distance = 0
+		@target = graph.target
+		@unvisited = (node for node in graph.nodes when node != @current)
+		@visited = []
+	
+	crawl: ->
+		
+		
+	visit: (current) ->
+		current.visited = true
+		@visited.push(current)
 		
--- js/dijkstra.js
+++ js/dijkstra.js
@@ -1,25 +1,80 @@
 (function() {
-  var Graph, Node;
+  var Dijkstra, Graph, Node;
 
   window.onload = function() {
-    var circle, graph, paper;
-    paper = Raphael("holder", 400, 400);
-    graph = new Graph(3, 3);
+    var graph, n1, n2, n3, n4, n5, n6, node, paper, _i, _len, _ref;
+    paper = Raphael("holder", 800, 600);
+    graph = new Graph(paper);
     window.graph = graph;
-    circle = paper.circle(50, 40, 10);
-    circle.attr("fill", "#f00");
-    return circle.attr("stroke", "#fff");
+    n1 = new Node(2, 4);
+    n1.origin = true;
+    n2 = new Node(4, 3);
+    n3 = new Node(2, 2);
+    n4 = new Node(4, 1);
+    n5 = new Node(2, 0);
+    n5.target = true;
+    n6 = new Node(0, 1);
+    n1.addEdge(7, n2);
+    n1.addEdge(14, n6);
+    n1.addEdge(9, n3);
+    _ref = [n1, n2, n3, n4, n5, n6];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      node = _ref[_i];
+      graph.addNode(node);
+    }
+    return graph.draw();
   };
 
   Graph = (function() {
 
     Graph.name = 'Graph';
 
-    function Graph(width, height) {
-      this.width = width;
-      this.height = height;
-      this.origin = new Node(this);
+    function Graph(raphael) {
+      this.raphael = raphael;
+      this.nodes = [];
+      this.origin = false;
+      this.target = false;
     }
+
+    Graph.prototype.addNode = function(node) {
+      this.nodes.push(node);
+      if (node.origin) {
+        this.origin = node;
+      }
+      if (node.target) {
+        return this.target = node;
+      }
+    };
+
+    Graph.prototype.draw = function(current) {
+      var node, _i, _len, _ref, _results;
+      if (current == null) {
+        current = false;
+      }
+      _ref = this.nodes;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        node = _ref[_i];
+        _results.push(this.drawNode(node));
+      }
+      return _results;
+    };
+
+    Graph.prototype.drawNode = function(node) {
+      var circle, color, x, y;
+      x = node.x * 65 + 65;
+      y = node.y * 65 + 65;
+      circle = this.raphael.circle(x, y, 30);
+      if (node === this.origin) {
+        color = "#fff";
+      } else if (node === this.target) {
+        color = "#f00";
+      } else {
+        color = '#00f';
+      }
+      circle.attr('fill', color);
+      return circle.attr('stroke', color);
+    };
 
     return Graph;
 
@@ -29,12 +84,57 @@
 
     Node.name = 'Node';
 
-    function Node(graph) {
-      this.graph = graph;
-      this.distance = -1;
+    function Node(x, y) {
+      this.x = x;
+      this.y = y;
+      this.visited = false;
+      this.distance = Infinity;
+      this.edges = {};
+      this.origin = false;
+      this.target = false;
     }
 
+    Node.prototype.addEdge = function(distance, node) {
+      return this.edges[node] = distance;
+    };
+
     return Node;
+
+  })();
+
+  Dijkstra = (function() {
+
+    Dijkstra.name = 'Dijkstra';
+
+    function Dijkstra(graph) {
+      var node;
+      this.graph = graph;
+      this.current = graph.origin;
+      this.curent.distance = 0;
+      this.target = graph.target;
+      this.unvisited = (function() {
+        var _i, _len, _ref, _results;
+        _ref = graph.nodes;
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          node = _ref[_i];
+          if (node !== this.current) {
+            _results.push(node);
+          }
+        }
+        return _results;
+      }).call(this);
+      this.visited = [];
+    }
+
+    Dijkstra.prototype.crawl = function() {};
+
+    Dijkstra.prototype.visit = function(current) {
+      current.visited = true;
+      return this.visited.push(current);
+    };
+
+    return Dijkstra;
 
   })();
 
